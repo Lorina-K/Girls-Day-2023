fbb9555d322fca11c78f7795dc34856bae2b5036
Peter Melms <peter.melms@cm4all.com>
2023-02-15 16:19:41 +0100

    fix(girlsDay-project): replaced evil jquery with vanilla-javascript ;-)
    - added .hidden class to hide/show elements

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-$(document).ready(function () {
-    $('.buttonDogs').click(function () {
-        $('.cat').hide();
-        $('.dog').show();
+// Sobald das Document vom Browser komplett geladen wurde, führe die folgenden Funktionen aus:
+document.addEventListener("DOMContentLoaded", function(){
 
-        $('.buttonDogs').addClass('active');
-        $('.buttonALL').removeClass('active');
-        $('.buttonCats').removeClass('active');
+    // Wähle den Button für Hundebilder aus und zeige nur Hunde an, dh blende Katzen aus
+    document.querySelector('.buttonDogs').addEventListener('click', function () {
+        // Wähle alle Elemente mit der Klasse '.cat' aus und blende sie aus
+        document.querySelectorAll('.cat').forEach(elem => elem.classList.add('hidden'));
+        // Wähle alle Elemente mit der Klasse '.dog' aus und zeige sie an
+        document.querySelectorAll('.dog').forEach(elem => elem.classList.remove('hidden'));
+
+        // Setze den Hunde-Button auf 'active'
+        document.querySelector('.buttonDogs').classList.add('active');
+        // Deaktiviere den Alle-Button
+        document.querySelector('.buttonALL').classList.remove('active');
+        // Deaktiviere den Katzen-Button
+        document.querySelector('.buttonCats').classList.remove('active');
     });
-    $('.buttonCats').click(function () {
-        $('.dog').hide();
-        $('.cat').show();
 
-        $('.buttonCats').addClass('active');
-        $('.buttonALL').removeClass('active');
-        $('.buttonDogs').removeClass('active');
+    // Wähle den Katzen-Button aus, und blende alle Hunde aus und alle Katzen ein
+    document.querySelector('.buttonCats').addEventListener('click', function() {
+        // Wähle alle Elemente mit der Klasse '.dog' aus und blende sie aus
+        document.querySelectorAll('.dog').forEach(elem => elem.classList.add('hidden'));
+        // Wähle alle Elemente mit der Klasse '.cat' aus und zeige sie an
+        document.querySelectorAll('.cat').forEach(elem => elem.classList.remove('hidden'));
+
+        // Setze den Katzen-Button auf 'active'
+        document.querySelector('.buttonCats').classList.add('active');
+        // Deaktiviere den Alle-Button
+        document.querySelector('.buttonALL').classList.remove('active');
+        // Deaktiviere den Hunde-Button
+        document.querySelector('.buttonDogs').classList.remove('active');
     });
-    $('.buttonALL').click(function () {
-        $('.dog').show();
-        $('.cat').show();
 
-        $('.buttonALL').addClass('active');
-        $('.buttonCats').removeClass('active');
-        $('.buttonDogs').removeClass('active');
+    document.querySelector('.buttonALL').addEventListener('click', function() {
+        // Wähle alle Elemente mit der Klasse '.dog' aus und zeige sie an
+        document.querySelectorAll('.dog').forEach(elem => elem.classList.remove('hidden'));
+        // Wähle alle Elemente mit der Klasse '.cat' aus und zeige sie an
+        document.querySelectorAll('.cat').forEach(elem => elem.classList.remove('hidden'));
+
+        // Aktiviere den Alle-Button
+        document.querySelector('.buttonALL').classList.add('active');
+        // Deaktiviere den Katzen-Button
+        document.querySelector('.buttonCats').classList.remove('active');
+        // Deaktiviere den Hunde-Button
+        document.querySelector('.buttonDogs').classList.remove('active');
     });
 });
